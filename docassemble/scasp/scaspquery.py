d7e1d4a14f8f8811c5c9468de4a54e2c144d912f
Jason Morris <jmorris@smu.edu.sg>
2021-04-25 12:54:43 +0800

    swithcing models to list of dict

--- docassemble/scasp/scaspquery.py
+++ docassemble/scasp/scaspquery.py
@@ -100,11 +100,11 @@ def sendQuery(filename, number=0):
                 if a['bindings'].sort() == na['bindings'].sort():
                     present = True
             if not present:
-                new_output['answers'].append({'bindings': a['bindings'], 'models': {}})
+                new_output['answers'].append({'bindings': a['bindings'], 'models': []})
         for a in output['answers']:
             for na in new_output['answers']:
                 if a['bindings'] == na['bindings']:
-                    na['models'] += {'time': a['time'], 'model': a['model'], 'explanations': a['explanations']}
+                    na['models'].append({'time': a['time'], 'model': a['model'], 'explanations': a['explanations']})
                     # na['models']['time'] = a['time']
                     # na['models']['model'] = a['model']
                     # na['models']['explanations'] = a['explanations']
